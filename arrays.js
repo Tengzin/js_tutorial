ba9605a1c57fb598f65ada6324d4835bd915234d
Teng Da Hu <tdhu@uwaterloo.ca>
2019-11-15 08:40:53 -0500

    Finish arrays

--- arrays.js
+++ arrays.js
@@ -11,9 +11,20 @@ for (let i = 0; i <  hb.length; i++) {
 }
 
 // Slicing
-let nums = [1, 2, 3, 4];
+let nums = [4, 3, 2, 1];
 console.log(nums.slice(1)); // slices off indices before 1
 console.log(nums.slice(1, 3)); // includes 1st but not 3rd
 console.log(nums.slice(-1)[0]); // last ele
 // Includes
 console.log(nums.includes(1));
+// Sort array (by alpha)
+console.log(nums.sort());
+// Reverse an array
+console.log(nums.reverse());
+// Push and pop
+nums.push(5); // push to back
+nums.pop(5); // removes last 
+nums.shift(); // removes first
+// Join
+console.log(nums.join()); // default is comma
+console.log(nums.join(", ")); // comma space
